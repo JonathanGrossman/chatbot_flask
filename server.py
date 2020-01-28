06b75927aad83cacd5984380344b48e5e24e0d9a
JonathanGrossman <jon@jonegrossman.com>
2020-01-28 06:01:47 -0600

    installed ability to save messages to db and respond accordingly

--- server.py
+++ server.py
@@ -12,12 +12,14 @@ from random import randrange
 app = Flask(__name__)
 CORS(app)
 
+
+response_message = {"message": "What did you say?"}
 urls = ['https://boto20.herokuapp.com/message/?message=', 'https://morning-basin-34003.herokuapp.com/message/?message=']
+
 
 connection = sqlite3.connect("database.db")
 cursor = connection.cursor()
-cursor.execute("SELECT * FROM messages")
-results = cursor.fetchall()
+
 
 def time_function():
     time_message = ""
@@ -28,38 +30,43 @@ def time_function():
     else:
         print("It's midnight!")
 
+
 scheduler = BackgroundScheduler()
 job = scheduler.add_job(time_function, 'interval', minutes=1)
 scheduler.start()
+
 
 @app.route("/", methods=['GET'])
 def hello_function(): 
     return redirect(url_for("message_handler"))
 
+
 @app.route("/message/", methods=['GET'])
 def message_handler():
+    global response_message
     random_number = randrange(11)
     random_number_urls = randrange(2)
     url = urls[random_number_urls]
-
+    
     if request.method == "GET":
+        user_input = request.args.get("message")
         connection = sqlite3.connect("database.db")
         cursor = connection.cursor()
         cursor.execute("SELECT * FROM messages")
         results = cursor.fetchall()
-        user_input = request.args.get("message")
-
+        print(results)
+        if user_input not in (item[0] for item in results):
+            cursor.execute('INSERT INTO messages VALUES (?)', ([user_input]))
+            connection.commit()     
         if random_number == 2:
-            r = requests.get(url + user_input)
-            json_string = json.loads(r.content)
+            response = requests.get(url + user_input)
+            json_string = json.loads(response.content)
             response_message = {"message": json_string["message"]}
         else:
-            for m in results:
-                if m[0] == user_input:
-                    response_message = {"message": "I've already answered that one."}
-                else:
-                    cursor.execute("INSERT INTO messages VALUES (?)", ([user_input]))
-                    response_message = {"message": "What did you say?"}
+            if user_input not in (item[0] for item in results):
+                response_message = {"message": "What did you say?"}
+            else:
+                response_message = {"message": "I've already answered that one."}
         return jsonify(response_message)
 
 
